711db7c4bdd5d516d8e7c052a047e36a3c409cc7
Dandara <dandaraleite2@gmail.com>
2023-09-01 10:46:31 -0300

    iterating over the products

--- weireless_keyboards_scraper.py
+++ weireless_keyboards_scraper.py
@@ -24,3 +24,16 @@ last_page = math.ceil(int(qtd) / 20)
 
 dic_product = {'brand':[], 'price': []}
 
+for page in range(1, last_page + 1):
+    url_page = f'https://www.kabum.com.br/perifericos/teclado-mouse/teclado-sem-fio?page_number={page}&page_size=20&facet_filters=&sort=most_searched'
+    site = requests.get(url_page, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+
+    products = soup.find_all('div', class_=re.compile('productCard'))
+
+    for product in products:
+        brand = product.find('span', class_=re.compile('nameCard')).get_text().strip()
+        price = product.find('span', class_=re.compile('priceCard')).get_text().strip()
+
+        print(brand, price)
+    print(url_page)
